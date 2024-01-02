1ebdfdd6b10a3a26b127140adca678bf29302fe8
Jaikishan Gupta <jaikishangupta754@gmail.comm>
2024-01-02 23:13:41 +0530

    sequelizers1

---
+++ util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+module.exports = sequelize;
